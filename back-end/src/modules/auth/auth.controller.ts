1c9cbd333cef211fb0cc0b2c74c237709e8ce23e
Joseph Alexander Londoño Moreno <joseph.londono@sofka.local>
2024-12-17 18:51:57 -0500

    docs(back-end): Add documentation in users and auth

--- back-end/src/modules/auth/auth.controller.ts
+++ back-end/src/modules/auth/auth.controller.ts
@@ -2,18 +2,59 @@ import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
 
 @Controller('/api/v1/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post()
+  @ApiOperation({
+    description: 'Permitted to login',
+  })
+  @ApiBody({
+    description: 'Permit the user to login',
+    type: AuthCredentialsDto,
+    examples: {
+      example1: {
+        value: {
+          email: 'example@email.com',
+          password: 'password',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'User logged in',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
   login(@Body() authCredentials: AuthCredentialsDto) {
     return this.authService.login(authCredentials);
   }
 
   @Get('data-user')
   @UseGuards(AuthGuard('jwt'))
+  @ApiOperation({
+    description: 'Permitted to get data user',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Data user',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
+  @ApiBearerAuth('jwt')
   dataUser(@Req() request) {
     return request.user;
   }
--- back-end/src/modules/users/users.controller.ts
+++ back-end/src/modules/users/users.controller.ts
@@ -11,42 +11,136 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserDto } from './dto/user.dto';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { ParseIntPipe } from '../../pipes/parse-int.pipe';
 import { UserDeleteDto } from './dto/user-delete.dto';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('/api/v1/users')
-@ApiTags('users')
+@ApiTags('Users')
 export class UsersController {
   constructor(private userService: UsersService) {}
 
   @Post()
   @UseGuards(AuthGuard('jwt'))
+  @ApiOperation({ description: 'Create a new user' })
+  @ApiBody({
+    description: 'Create a new user',
+    type: UserDto,
+    examples: {
+      example1: {
+        value: {
+          name: 'John Doe',
+          email: 'johnDoe@gmail.com',
+          password: '1234',
+        },
+      },
+    },
+  })
+  @ApiBearerAuth('jwt')
+  @ApiResponse({
+    status: 201,
+    description: 'The user has been successfully created.',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'The user already exists.',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
   async createUser(@Body() user: UserDto) {
     return await this.userService.createUser(user);
   }
 
   @Get()
   @UseGuards(AuthGuard('jwt'))
+  @ApiOperation({ description: 'Get all users' })
+  @ApiBearerAuth('jwt')
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully retrieved users.',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
   async getUsers() {
     return await this.userService.getUsers();
   }
 
   @Delete('/clear')
   @UseGuards(AuthGuard('jwt'))
+  @ApiOperation({ description: 'Clear all users' })
+  @ApiBearerAuth('jwt')
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully cleared users.',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
   async clearUsers() {
     return await this.userService.clearUsers();
   }
 
   @Delete('/:id')
   @UseGuards(AuthGuard('jwt'))
+  @ApiOperation({ description: 'Delete a user by ID' })
+  @ApiBearerAuth('jwt')
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully deleted user.',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'User not found.',
+  })
   async deleteUserById(@Param('id', ParseIntPipe) id: number) {
     return await this.userService.deleteUserById(id);
   }
 
   @Patch()
   @UseGuards(AuthGuard('jwt'))
+  @ApiOperation({ description: 'Update a user' })
+  @ApiBody({
+    description: 'Update a user',
+    type: UserDeleteDto,
+    examples: {
+      example1: {
+        value: {
+          id: 1,
+          name: 'John Doe Updated',
+          email: 'johnDoeUpdated@gmail.com',
+        },
+      },
+    },
+  })
+  @ApiBearerAuth('jwt')
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully updated user.',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'User not found.',
+  })
   async updateUser(@Body() user: UserDeleteDto) {
     return await this.userService.updateUser(user);
   }
