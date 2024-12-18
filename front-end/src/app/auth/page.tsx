20ec67da154bf6524076a703a1e6287d3eb46d90
Joseph Alexander Londoño Moreno <joseph.londono@sofka.local>
2024-12-17 21:40:50 -0500

    feat(front-end): Redirect to home page if user logged

--- front-end/src/app/auth/page.tsx
+++ front-end/src/app/auth/page.tsx
@@ -1,8 +1,15 @@
+"use client";
 import Header from "../components/index/Header";
 import Footer from "../components/index/Footer";
 import { FormsAuth } from "../components/auth/FormsAuth";
+import { getCookie } from "cookies-next/client";
 
 export default function Auth() {
+  const token = getCookie("accesToken");
+
+  if (token) {
+    window.location.href = "/";
+  }
   return (
     <div className="min-h-screen flex flex-col gap-3 bg-sofka-light">
       <Header />
