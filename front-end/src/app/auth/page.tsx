a4609b2917174ad9d946fbe0466efdf2d853478b
Joseph Alexander Londoño Moreno <joseph.londono@sofka.local>
2024-12-26 16:28:04 -0500

    chore(front-end): modify page auth for login with google

--- front-end/src/app/auth/page.tsx
+++ front-end/src/app/auth/page.tsx
@@ -1,30 +1,11 @@
 "use client";
-import { useState } from "react";
-import { getCookie } from "cookies-next/client";
 import { SectionLogin } from "@/components/auth/SectionLogin";
-import SectionRegister from "@/components/auth/SectionRegister";
 
 export default function Auth() {
-  const [sectionLogin, setSectionLogin] = useState(true);
-  const token = getCookie("accesToken");
-
-  if (token) {
-    window.location.href = "/";
-  }
   return (
     <main className="flex-1 flex justify-center items-center">
       <section className="max-w-7xl flex flex-col w-full mx-auto gap-y-4 py-4">
-        {sectionLogin ? (
-          <SectionLogin
-            setSectionLogin={setSectionLogin}
-            sectionLogin={sectionLogin}
-          />
-        ) : (
-          <SectionRegister
-            setSectionLogin={setSectionLogin}
-            sectionLogin={sectionLogin}
-          />
-        )}
+        <SectionLogin />
       </section>
     </main>
   );
--- front-end/src/components/auth/SectionLogin.tsx
+++ front-end/src/components/auth/SectionLogin.tsx
@@ -1,162 +1,63 @@
-import { setCookie } from "cookies-next/client";
+import { signIn } from "next-auth/react";
 import Image from "next/image";
-import Link from "next/link";
-import React, { useState } from "react";
-import { jwtDecode } from "jwt-decode";
-import { TokenPayload } from "@/types/TokenPayload";
 
-interface SectionLoginProps {
-  setSectionLogin: (value: boolean) => void;
-  sectionLogin: boolean;
-}
-
-const getExpirationDate = (token: string): Date | null => {
-  try {
-    const decoded = jwtDecode<TokenPayload>(token);
-    if (decoded.exp) {
-      const expirationDate = new Date(decoded.exp * 1000);
-      return expirationDate;
-    }
-    return null;
-  } catch (error) {
-    console.error("Error decoding token:", error);
-    return null;
-  }
-};
-
-const getMaxAge = (expirationDate: Date): number => {
-  const now = new Date();
-  const maxAge = Math.floor((expirationDate.getTime() - now.getTime()) / 1000);
-  return maxAge;
-};
-
-export const SectionLogin: React.FC<SectionLoginProps> = ({
-  setSectionLogin,
-  sectionLogin,
-}) => {
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    setIsSubmitting(true);
-    e.preventDefault();
-    const email = (e.currentTarget[0] as HTMLInputElement).value;
-    const password = (e.currentTarget[1] as HTMLInputElement).value;
-
-    const credentials = {
-      email,
-      password,
-    };
-
-    try {
-      const response = await fetch(
-        `${
-          process.env.NEXT_PUBLIC_URL_PROD_BACKEND ||
-          process.env.NEXT_PUBLIC_URL_DEV_BACKEND
-        }/api/v1/auth`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(credentials),
-        }
-      );
-      const messageResponse = await response.json();
-
-      if (!response.ok) {
-        throw new Error(messageResponse.message);
-      }
-      if (typeof messageResponse.accesToken === "string") {
-        const expirationDate = getExpirationDate(messageResponse.accesToken);
-
-        if (expirationDate) {
-          const maxAge = getMaxAge(expirationDate);
-          setCookie("accesToken", messageResponse.accesToken, { maxAge });
-        } else {
-          setCookie("accesToken", messageResponse.accesToken);
-        }
-        window.location.href = "/";
-      } else {
-        throw new Error("Invalid token format");
-      }
-    } catch (error) {
-      const mError = error as Error;
-      setMessageError(mError.message);
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
-
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [messageError, setMessageError] = useState("");
+export const SectionLogin = ({}) => {
   return (
-    <div className="flex mx-auto bg-white rounded-xl shadow-2xl overflow-hidden lg:max-w-4xl lg:w-full w-[90%]">
-      <div className="p-7 flex-1">
-        <h2 className="text-3xl font-bold text-gray-800 mb-1">
-          Iniciar Sesión
-        </h2>
-        <p className="max-w-xs text-sm text-gray-600">
-          Por favor, ingrese sus credenciales para poder usar la aplicación
-        </p>
-        <hr className="border-t-2 border-gray-200 my-3" />
-        <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
-          <label className="flex flex-col gap-1">
-            <span className="text-sm font-semibold text-gray-700">Correo</span>
-            <input
-              type="email"
-              placeholder="Ingrese su correo electrónico"
-              required
-              className="w-full rounded-lg p-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-sofka-orange/50 transition-all duration-300 text-gray-700 placeholder-gray-400"
-            />
-          </label>
-          <label className="flex flex-col gap-1">
-            <span className="text-sm font-semibold text-gray-700">
-              Contraseña
-            </span>
-            <input
-              type="password"
-              placeholder="Ingrese su contraseña"
-              required
-              className="w-full rounded-lg p-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-sofka-orange/50 transition-all duration-300 text-gray-700 placeholder-gray-400"
-            />
-          </label>
-          <div className="flex justify-end items-center">
-            <Link
-              href="/auth/recover-password"
-              className="text-sm text-sofka-orange hover:underline"
-            >
-              ¿Olvidaste tu contraseña?
-            </Link>
-          </div>
-          <button
-            type="submit"
-            className="bg-sofka-orange text-white py-2 px-1 rounded-lg font-semibold hover:bg-opacity-90 transition-all duration-300 ease-in-out transform hover:-translate-y-1 shadow-md hover:shadow-lg disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:translate-y-0"
-            disabled={isSubmitting}
-          >
-            {isSubmitting ? "Iniciando sesion" : "Iniciar sesion"}
-          </button>
-          <span
-            className={`w-full bg-red-200 px-3 py-1 rounded-xl text-xs text-red-700 text-center max-w-[70%] mx-auto mt-2 ${
-              messageError ? "block" : "hidden"
-            }`}
-          >
-            {messageError}
-          </span>
-          <p className="text-xs text-gray-600 text-center">
-            ¿No tienes cuenta?{" "}
-            <button
-              type="button"
-              className="hover:underline text-sofka-orange hover:text-sofka-orange/80"
-              onClick={() => setSectionLogin(!sectionLogin)}
-            >
-              Registrate aqui
-            </button>
+    <div className="flex mx-auto bg-white rounded-xl shadow-2xl overflow-hidden lg:max-w-4xl lg:w-full w-[90%] gap-x-8">
+      <div className="p-7 flex-1 flex flex-col">
+        <div className="flex-grow">
+          <h2 className="text-3xl font-bold text-gray-800 mb-1">
+            Iniciar Sesión
+          </h2>
+          <p className="max-w-xs text-sm text-gray-600">
+            Por favor, ingrese sesion en google para continuar
           </p>
-        </form>
+          <hr className="border-t-2 border-gray-200 my-3" />
+          <div className="flex-1">
+            <button
+              className="flex items-center gap-2 bg-white border border-gray-300 rounded-md p-2 w-full justify-center font-semibold text-gray-600 hover:bg-gray-100 hover:border-gray-400"
+              onClick={() => signIn("google")}
+            >
+              <svg
+                width="17"
+                height="17"
+                viewBox="0 0 256 262"
+                xmlns="http://www.w3.org/2000/svg"
+                preserveAspectRatio="xMidYMid"
+              >
+                <path
+                  d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
+                  fill="#4285F4"
+                />
+                <path
+                  d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"
+                  fill="#34A853"
+                />
+                <path
+                  d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782"
+                  fill="#FBBC05"
+                />
+                <path
+                  d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"
+                  fill="#EB4335"
+                />
+              </svg>{" "}
+              Continúa con google
+            </button>
+          </div>
+        </div>
+        <div className="mt-auto">
+          <hr className="border-t-2 border-gray-200 my-3" />
+          <p className="text-center text-gray-600 text-sm">
+            Por su seguridad, el inicio de sesión se realiza a través de Google.
+          </p>
+        </div>
       </div>
       <div className="relative w-[400px] md:block hidden">
         <div className="absolute inset-0 bg-[#ebe8ff] transform rotate-6 scale-150 ml-20"></div>
         <div className="relative z-10 items-center justify-center flex h-full">
           <Image
-            src="/login.png"
+            src="/pcVector.svg"
             alt="Image of a login"
             width={370}
             height={370}
