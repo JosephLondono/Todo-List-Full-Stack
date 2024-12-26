fc7fbfad63a8394eea7149c2919f7030592be89e
Joseph Alexander Londoño Moreno <joseph.londono@sofka.local>
2024-12-26 16:53:43 -0500

    chore(front-end): add event signin

--- front-end/src/app/api/auth/[...nextauth]/route.ts
+++ front-end/src/app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,34 @@ const handle = NextAuth({
       clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
     }),
   ],
+  events: {
+    signIn: async (message) => {
+      try {
+        const response = await fetch(
+          `${
+            process.env.NEXT_PUBLIC_URL_PROD_BACKEND ||
+            process.env.NEXT_PUBLIC_URL_DEV_BACKEND
+          }/api/v1/users`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email: message.user.email,
+              name: message.user.name,
+              image: message.user.image,
+            }),
+          }
+        );
+        const data = await response.json();
+        if (data.message === "El usuario con el email proporcionado ya existe")
+          return;
+      } catch (error) {
+        console.error("signIn", error);
+      }
+    },
+  },
 });
 
 export { handle as GET, handle as POST };
