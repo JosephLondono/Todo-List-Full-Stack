c8620166aa8036ff3fdb7018b0212d4521e88fe4
Joseph Alexander Londoño Moreno <joseph.londono@sofka.local>
2024-12-26 19:02:54 -0500

    chore(front-end): add middleware

---
+++ front-end/src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+const protectedRoutes = ["/task", "/settings"];
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req });
+  const { pathname } = req.nextUrl;
+
+  console.log("middleware", { token, pathname });
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/auth"));
+  }
+
+  if (protectedRoutes.includes(pathname)) {
+    console.log("middleware", "redirecting to /auth");
+
+    return NextResponse.redirect(new URL("/auth", req.nextUrl.origin));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/data|favicon.ico).*)"],
+};
