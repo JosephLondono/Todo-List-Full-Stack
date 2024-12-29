3af815a38f9a326fea863d912a1a922e599b7ffb
Joseph Alexander Londoño Moreno <joseph.londono@sofka.local>
2024-12-28 22:06:43 -0500

    chore(front-end): separate loader per component

---
+++ front-end/src/components/Loader.tsx
@@ -0,0 +1,8 @@
+import "@/components/Loader.css";
+const Loader = () => {
+  return (
+    <div className="loader absolute z-50 flex items-center justify-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+  );
+};
+
+export default Loader;
