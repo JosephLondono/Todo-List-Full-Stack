fa7cd3fa75478318adfb8e6298f100e43375e672
Joseph Alexander Londoño Moreno <joseph.londono@sofka.local>
2024-12-30 13:52:43 -0500

    chore(front-end): add dark mode in page auth

--- front-end/src/app/auth/page.tsx
+++ front-end/src/app/auth/page.tsx
@@ -3,7 +3,7 @@ import { SectionLogin } from "@/components/auth/SectionLogin";
 
 export default function Auth() {
   return (
-    <main className="flex-1 flex justify-center items-center">
+    <main className="flex-1 flex justify-center items-center bg-white dark:bg-gray-900">
       <section className="max-w-7xl flex flex-col w-full mx-auto gap-y-4 py-4">
         <SectionLogin />
       </section>
