65ca07216960d48fa55e66940c794e8ddf4d6835
Joseph Alexander Londoño Moreno <joseph.londono@sofka.local>
2024-12-30 13:55:14 -0500

    chore(front-end): add dark mode on button auth

--- front-end/src/components/ButtonAuth.tsx
+++ front-end/src/components/ButtonAuth.tsx
@@ -3,7 +3,7 @@ import { useSession, signOut } from "next-auth/react";
 export default function ButtonAuth() {
   const { data: session } = useSession();
   const classButton =
-    "bg-black rounded-md px-6 py-2 text-white hover:bg-gray-800 text-base h-10 block";
+    "bg-black rounded-md px-6 py-2 text-white hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-300 text-base h-10 block font-medium";
   if (session) {
     return (
       <>
